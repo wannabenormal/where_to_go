06c512a139263aa5acd4a75294a6f2c35903bcbd
Alexey Pesotsky <pesthedog@mail.ru>
2022-05-13 14:01:35 +0400

    Show image preview in Place admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
-admin.site.register(Place)
+
+class ImagesInline(admin.TabularInline):
+    model = Image
+    fields = ['image', 'preview_image', 'order']
+    readonly_fields = ['preview_image']
+    extra = 1
+
+    def preview_image(self, obj):
+        return format_html('<img src="{}" height="200"/>', obj.image.url)
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImagesInline]
+
+
 admin.site.register(Image)
